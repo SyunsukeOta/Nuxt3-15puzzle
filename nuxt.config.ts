75e305f77f8b673eb557c0117d5f0aca21e9b2e9
kibaota <ohta.syunsuke@liveout.co.jp>
2024-07-18 13:57:36 +0900

    前のプロジェクトから引き継ぎ。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	css: [
+		"@/assets/css/block.css",
+		"@/assets/css/button.css",
+		"@/assets/css/color.css",
+		"@/assets/css/frame.css",
+		"@/assets/css/home.css",
+		"@/assets/css/play.css",
+		"@/assets/css/result.css",
+		"@/assets/css/text.css",
+	]
 })
---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+	<section>
+		<p class="title tx_black" id="fifpuzzle">15パズル</p>
+		<div class="text tx_black" id="desc">15パズルは、<br>ブロックをスライドさせて<br>この形にするゲームです。</div>
+		<nuxt-link to="play">
+			<div class="black button" id="startbutton">
+				<div class="bt_text tx_white text">始める</div>
+			</div>
+		</nuxt-link>
+		<div>
+			<Block id="right"/>
+			<!--<div class="gray frout_black frame" id="right">
+				<div class="container">
+					<div class="block blue">
+						<div class="text tx_white bl_text bold">1</div>
+					</div>
+					<div class="block blue">
+						<div class="text tx_white bl_text bold">2</div>
+					</div>
+					<div class="block blue">
+						<div class="text tx_white bl_text bold">3</div>
+					</div>
+					<div class="block blue">
+						<div class="text tx_white bl_text bold">4</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="block orange">
+						<div class="text tx_white bl_text bold">5</div>
+					</div>
+					<div class="block orange">
+						<div class="text tx_white bl_text bold">6</div>
+					</div>
+					<div class="block orange">
+						<div class="text tx_white bl_text bold">7</div>
+					</div>
+					<div class="block orange">
+						<div class="text tx_white bl_text bold">8</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="block yellow">
+						<div class="text tx_white bl_text bold">9</div>
+					</div>
+					<div class="block yellow">
+						<div class="text tx_white bl_text bold">10</div>
+					</div>
+					<div class="block yellow">
+						<div class="text tx_white bl_text bold">11</div>
+					</div>
+					<div class="block yellow">
+						<div class="text tx_white bl_text bold">12</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="block green">
+						<div class="text tx_white bl_text bold">13</div>
+					</div>
+					<div class="block green">
+						<div class="text tx_white bl_text bold">14</div>
+					</div>
+					<div class="block green">
+						<div class="text tx_white bl_text bold">15</div>
+					</div>
+					<div class="block">
+					</div>
+				</div>
+			</div>-->
+		</div>
+	</section>
+</template>
+<script>
+//export default defineNuxtConfig({
+//	head: {
+//		bodyAttrs: {
+//			class: 'white'
+//		}
+//	}
+//})
+</script>
